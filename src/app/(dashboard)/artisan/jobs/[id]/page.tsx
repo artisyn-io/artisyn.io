cfd0248c288c3cfe06fb23bc93b032d074bec29d
akprinciple <akeemolayiwola9@gmail.com>
2026-06-01 00:57:15 +0100

    feat(frontend): add job details page

--- src/app/(dashboard)/artisan/jobs/[id]/page.tsx
+++ src/app/(dashboard)/artisan/jobs/[id]/page.tsx
@@ -19,6 +19,7 @@ export default function JobDetailsPage({ params }: JobDetailsPageProps) {
   // }
 
   return (
+    
     <main className="my-8 px-4">
       <div className="mb-6 flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
         <div>
